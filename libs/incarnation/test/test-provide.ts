24372524c4d76c5a33ed6913ba5ebcc6fa3c55ce
dfahlander <david.fahlander@gmail.com>
2020-02-28 07:44:51 +0100

    Unit test to test DI

---
+++ libs/incarnation/test/test-provide.ts
@@ -0,0 +1,60 @@
+import { entryPoint } from "../src/entryPoint";
+import { provide } from "../src/provide";
+import { inject } from "../src/inject";
+
+describe("provide", () => {
+  it("should be possible to provide an alternate implementation of the service itself when invoked using entryPoint", async () => {
+    class MyService {
+      sayHi() {
+        return "Hi!";
+      }
+    }
+
+    class MyAlternateService {
+      sayHi() {
+        return "Ho!";
+      }
+    }
+
+    console.log("Before inject");
+    const myService = inject(MyService);
+    console.log("After inject");
+    provide(myService).with((Class, next) =>
+      Class === MyService ? MyAlternateService : next(Class)
+    );
+    console.log("After provide");
+
+    expect(myService.sayHi()).toBe("Ho!");
+  });
+
+  /*it("should be possible to provide an alternate implementation for a dependency when main class invoked using entryPoint", async () => {
+    class MyService {
+      sayHi(num: number) {
+        const myDep = inject(MyDep);
+        return `${myDep.bark()} ${num}`;
+      }
+    }
+
+    class MyDep {
+      bark() {
+        return "bowow";
+      }
+    }
+
+    class AlternateDep extends MyDep {
+      bark() {
+        return "bohow";
+      }
+    }
+
+    const myService = entryPoint(MyService);
+    provide(myService).with((Class, next) =>
+      Class === MyDep ? AlternateDep : next(Class)
+    );
+
+    const resultPromise = myService.sayHi(1);
+    expect(resultPromise).toBeInstanceOf(Promise);
+    const result = await resultPromise;
+    expect(result).toBe("bohow 1");
+  });*/
+});
