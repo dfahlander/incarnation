f0abf0d162ba8253a24985925f055088e624507c
dfahlander <david.fahlander@gmail.com>
2020-03-02 21:09:42 +0100

    More test cases. Succeeding.

--- libs/incarnation/test/test-provide.ts
+++ libs/incarnation/test/test-provide.ts
@@ -3,19 +3,31 @@ import { provide } from "../src/provide";
 import { inject } from "../src/inject";
 
 describe("provide", () => {
+  class MyService {
+    sayHi() {
+      return "Hi!";
+    }
+  }
+
+  class MyAlternateService extends MyService {
+    sayHi() {
+      return "Ho!";
+    }
+  }
+
+  class MyAlternateService2 extends MyService {
+    sayHi() {
+      return "Hey!";
+    }
+  }
+
+  class MyAlternativeToMyAlternateService extends MyAlternateService {
+    sayHi() {
+      return "Hohoho!";
+    }
+  }
+
   it("should be possible to provide an alternate implementation of the service itself when invoked using entryPoint", async () => {
-    class MyService {
-      sayHi() {
-        return "Hi!";
-      }
-    }
-
-    class MyAlternateService {
-      sayHi() {
-        return "Ho!";
-      }
-    }
-
     const myService = inject(MyService);
     provide(myService).with((requestedClass, mappedClass, next) =>
       next(
@@ -26,7 +38,22 @@ describe("provide", () => {
     expect(myService.sayHi()).toBe("Ho!");
   });
 
-  /*it("should be possible to provide an alternate implementation for a dependency when main class invoked using entryPoint", async () => {
+  it("should prioritize the latest provided implementation", () => {
+    const myService = inject(MyService);
+    provide(myService).with(MyAlternateService);
+    expect(myService.sayHi()).toBe("Ho!");
+    provide(myService).with(MyAlternateService2);
+    expect(myService.sayHi()).toBe("Hey!");
+  });
+
+  it("should be possible to provide alternatives for concrete classes", () => {
+    const myService = inject(MyService);
+    provide(myService).with(MyAlternativeToMyAlternateService);
+    provide(myService).with(MyAlternateService);
+    expect(myService.sayHi()).toBe("Hohoho!");
+  });
+
+  it("should be possible to provide an alternate implementation for a dependency when main class invoked using entryPoint", async () => {
     class MyService {
       sayHi(num: number) {
         const myDep = inject(MyDep);
@@ -47,13 +74,11 @@ describe("provide", () => {
     }
 
     const myService = entryPoint(MyService);
-    provide(myService).with((Class, next) =>
-      Class === MyDep ? AlternateDep : next(Class)
-    );
+    provide(myService).with(AlternateDep);
 
     const resultPromise = myService.sayHi(1);
     expect(resultPromise).toBeInstanceOf(Promise);
     const result = await resultPromise;
     expect(result).toBe("bohow 1");
-  });*/
+  });
 });
