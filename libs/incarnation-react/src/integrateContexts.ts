eea088f2db9f12c160479e7312131621a0cc8c8d
dfahlander <david.fahlander@gmail.com>
2020-08-27 09:41:01 +0200

    Not yet used function to integrate contexts without using readContext()

---
+++ libs/incarnation-react/src/integrateContexts.ts
@@ -0,0 +1,15 @@
+import { Context } from "incarnation";
+import { IncarnationReactContext } from "incarnation-react/src/IncarnationReactContext";
+
+export function integrateContexts() {
+  Context.integrate((next) => {
+    const reactCtx = (IncarnationReactContext as any)._currentValue;
+    return reactCtx === Context.base ? next() : reactCtx;
+  });
+}
+/*
+  Koden används ännu inte.
+  Idén är att en react komponent utanför <Observe> ändå ska fungera men som statisk cachande resultat.
+  Då måste react-incarnation ha importerats. Annars går det ju inte alls. Eller?
+  Jo, om vi kan låta Context.root gälla. Och cacha saker globalt. Så ... jo.
+*/
